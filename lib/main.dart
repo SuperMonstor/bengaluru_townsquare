2637b34459c1870f5d0eafee2bab3b40baecdb46
Sudarshan <senthilsudarshan@gmail.com>
2024-04-08 00:35:49 +0530

    fixed orientation

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,19 @@ import 'package:bengaluru_townsquare/screens/auth/auth1_welcome.dart';
 import 'package:bengaluru_townsquare/theme/palette.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
+  // Firebase stuff
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Fix portrait mode
+    SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MyApp());
 }
 
